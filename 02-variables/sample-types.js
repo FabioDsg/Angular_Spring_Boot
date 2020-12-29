c0a2a5eba0e06f1ab40bb2139693a139211a3798
Fábio <fdsgoncalves@outlook.com>
2020-12-28 22:50:58 -0300

    Variables sample types

---
+++ 02-variables/sample-types.js
@@ -0,0 +1,9 @@
+var found = true;
+var grade = 88.3;
+var firstName = "Jonh";
+var lastName = "Doe";
+console.log(found);
+console.log("The grade is: " + grade);
+console.log("Hi " + firstName + " " + lastName);
+// Use template string
+console.log("Hi " + firstName + " " + lastName);
---
+++ 02-variables/sample-types.ts
@@ -0,0 +1,11 @@
+let found: boolean = true;
+let grade: number = 88.3;
+let firstName: string = "Jonh";
+let lastName: string = "Doe";
+
+console.log(found);
+console.log("The grade is: " + grade);
+console.log("Hi " + firstName + " " + lastName);
+
+// Use template string
+console.log(`Hi ${firstName} ${lastName}`);
