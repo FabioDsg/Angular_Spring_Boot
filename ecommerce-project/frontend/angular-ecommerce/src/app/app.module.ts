1670fdaac27ddf65b828187a94a268d142911063
Fábio <fdsgoncalves@outlook.com>
2021-01-09 10:47:20 -0300

    Build URL based on category id

--- ecommerce-project/frontend/angular-ecommerce/src/app/app.module.ts
+++ ecommerce-project/frontend/angular-ecommerce/src/app/app.module.ts
@@ -7,12 +7,25 @@ import { ProductListComponent } from './components/product-list/product-list.com
 import { HttpClientModule } from "@Angular/common/http";
 import { ProductService } from './services/product.service';
 
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+
+  {path: 'category/:id', component: ProductListComponent},
+  {path: 'category', component: ProductListComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: '', redirectTo: '/products', pathMatch:'full'},
+  {path: '**', redirectTo: '/products', pathMatch:'full'}
+
+];
+
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule
   ],
