4b512f29bfaf464f0ba5cf1858562cd3d9a42d8e
Fábio <fdsgoncalves@outlook.com>
2021-02-06 14:13:17 -0300

    Update database

--- ecommerce-project/backend/spring-boot-ecommerce/src/main/java/com/bootspring/ecommerce/entity/Product.java
+++ ecommerce-project/backend/spring-boot-ecommerce/src/main/java/com/bootspring/ecommerce/entity/Product.java
@@ -5,13 +5,18 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
 
 @Entity
 @Table(name="product")
 @Data
-public class Product {
+public class Product implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
